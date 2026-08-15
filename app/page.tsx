7f9da43a040e468712e150f34b2e19b1c8330641
Arnav Kshirsagar <arnavk0807@gmail.com>
2026-08-15 10:13:49 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Hero />
+      <About />
+      <Skills />
+      <Footer />
+    </main>
+  );
+}
